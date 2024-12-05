0b468d6373e6cd463c4f7b259467abd78f37ab0a
bygu4 <sacha05bugaev@gmail.com>
2024-12-05 19:01:28 +0300

    add tests

--- Tasks/Test2/Test2.Tests/ReflectorTest.cs
+++ Tasks/Test2/Test2.Tests/ReflectorTest.cs
@@ -1,14 +1,24 @@
 namespace Test2.Tests;
 
+using NUnit.Framework;
+
 public static class ReflectorTest
 {
-    private const string ExpectedOutputPath1 = "ExpectedOutput/Output1.txt"
+    private const string ExpectedOutputPath1 = "ExpectedOutput/Output1.txt";
+    private const string ResultPath = "TestClass.cs";
 
     [Test]
     public static async Task TestReflector_PrintStructure_CompareResults()
     {
-        var expectedOutput = await File.ReadAllTextAsync(ExpectedOutputPath1)
+        var expectedOutput = await File.ReadAllTextAsync(ExpectedOutputPath1);
         await Reflector.PrintStructure(typeof(TestClass1));
-        
+        var actualOutput = await File.ReadAllTextAsync(ResultPath);
+        Assert.That(actualOutput, Is.EqualTo(expectedOutput));
+    }
+
+    public static async Task TestReflector_PrintSomeTypes()
+    {
+        await Reflector.PrintStructure(typeof(Int32));
+        await Reflector.PrintStructure(typeof(String));
     }
 }
