ea61f96a8058e360721ddce73347ef296518879c
bygu4 <sacha05bugaev@gmail.com>
2024-12-17 20:26:34 +0300

    update license headers, enable stylecop in tests

--- Tasks/MyNUnit/MyNUnit.Tests/MyNUnitCoreTest.cs
+++ Tasks/MyNUnit/MyNUnit.Tests/MyNUnitCoreTest.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2024
+// Copyright (c) Alexander Bugaev 2024
 //
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file or at
@@ -8,6 +8,9 @@ namespace MyNUnit.Core.Tests;
 
 using NUnit.Framework;
 
+/// <summary>
+/// Tests for MyNUnit framework.
+/// </summary>
 public static class MyNUnitCoreTest
 {
     private const string TestProjectsPath = "TestProjects/";
@@ -23,26 +26,14 @@ public static class MyNUnitCoreTest
         (2, 1, 1),
     ];
 
-    private static string[] TestDirectoryPaths
-    {
-        get
-        {
-            var paths = Directory.GetDirectories(TestProjectsPath);
-            Array.Sort(paths);
-            return paths;
-        }
-    }
-
-    private static IEnumerable<TestCaseData> CorrectnessTestCases()
-    {
-        for (int i = 0; i < testResults.Count; ++i)
-        {
-            var testDirectoryPath = TestDirectoryPaths[i];
-            var (passed, failed, ignored) = testResults[i];;
-            yield return new TestCaseData(testDirectoryPath, passed, failed, ignored);
-        }
-    }
-
+    /// <summary>
+    /// Tests the correctness of the testing.
+    /// </summary>
+    /// <param name="path">Path to search for assemblies at.</param>
+    /// <param name="testsPassed">Expected number of tests to pass.</param>
+    /// <param name="testsFailed">Expected number of tests to fail.</param>
+    /// <param name="testsIgnored">Expected number of tests to be ignored.</param>
+    /// <returns>Task representing the test.</returns>
     [TestCaseSource(nameof(CorrectnessTestCases))]
     public static async Task TestCorrectness_RunTestsForEachAssembly_TestResultsAreCorrect(
         string path,
@@ -56,6 +47,11 @@ public static class MyNUnitCoreTest
         Assert.That(testResult.NumberOfTestsIgnored, Is.EqualTo(testsIgnored));
     }
 
+    /// <summary>
+    /// Tests the consistency of the test results.
+    /// </summary>
+    /// <param name="path">Path to search for assemblies at.</param>
+    /// <returns>Task representing the test.</returns>
     [TestCaseSource(nameof(TestDirectoryPaths))]
     public static async Task TestConsistency_RunTestsTwice_TestResultsAreEqual(string path)
     {
@@ -64,8 +60,29 @@ public static class MyNUnitCoreTest
         Assert.That(testResult1, Is.EqualTo(testResult2));
     }
 
+    /// <summary>
+    /// Tests the testing for non existing directory.
+    /// </summary>
     [TestAttribute]
     public static void TestStability_RunTestsForNonExistentDirectory_ThrowException()
         => Assert.ThrowsAsync<DirectoryNotFoundException>(
             () => MyNUnitCore.RunTestsFromEachAssembly("mboieuibnui"));
+
+    private static string[] TestDirectoryPaths()
+    {
+        var paths = Directory.GetDirectories(TestProjectsPath);
+        Array.Sort(paths);
+        return paths;
+    }
+
+    private static IEnumerable<TestCaseData> CorrectnessTestCases()
+    {
+        var testDirectoryPaths = TestDirectoryPaths();
+        for (int i = 0; i < testResults.Count; ++i)
+        {
+            var testDirectoryPath = testDirectoryPaths[i];
+            var (passed, failed, ignored) = testResults[i];
+            yield return new TestCaseData(testDirectoryPath, passed, failed, ignored);
+        }
+    }
 }
